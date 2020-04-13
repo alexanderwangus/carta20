4bd5584976c739a3bb50cae22a6318c74ce18d0e
alexanderwangus <aswang@stanford.edu>
2020-04-13 14:33:17 -0700

    polished notebook

--- computer_science.py
+++ computer_science.py
@@ -1,4 +1,28 @@
-tracks = {}
+from req_builder import Req, Subreq
+
+TRACKS = {}
+TRACKNAMES = [
+    'graphics',
+    'compeng',
+    'biocomp',
+    'hci',
+    'info',
+    'systems',
+    'theory',
+    'ai',
+    'unspecialized'
+]
+TRACK_PRIORITIES = {
+    'graphics': 2,
+    'compeng': 1,
+    'biocomp': 3,
+    'hci': 4,
+    'info': 7,
+    'systems': 8,
+    'theory': 5,
+    'ai': 6,
+    'unspecialized': 9
+}
 
 genelec = Req([
     'CS108', 'CS124', 'CS131', 'CS140', 'CS142', 'CS143', 'CS144', 'CS145', 'CS147', 'CS148', 'CS149', 'CS154', 'CS155',
@@ -20,11 +44,11 @@ Graphics
 graphics_reqB = Req(['CS205A', 'CME104', 'CME108', 'MATH52', 'MATH113'], 1)
 graphics_reqC = Req([Req(['CS131', 'CS231A'], 1),
            'CS233', 'CS268', 'CS348A', 'CS348B', 'CS348C', 'CS448'], 2)
-tracks['graphics'] = Req([
+TRACKS['graphics'] = Req([
     'CS148', 'CS248',                  # Track Requirement A
-    reqB,                              # Track Requirement B
-    reqC,                              # Track Requirement C
-    Subreq([reqB, reqC, genelec],
+    graphics_reqB,                              # Track Requirement B
+    graphics_reqC,                              # Track Requirement C
+    Subreq([graphics_reqB, graphics_reqC, genelec],
            ['ARTSTUDI160', 'ARTSTUDI160170', 'ARTSTUDI160179',
             'CME302', 'CME306',
             'EE168', 'EE262', 'EE264', 'EE278', 'EE368',
@@ -36,7 +60,7 @@ tracks['graphics'] = Req([
 '''
 Computational Engineering
 '''
-tracks['compeng'] = Req([
+TRACKS['compeng'] = Req([
     'EE108', # A
     'EE180', # A
     Req(['EE101A', 'EE101B', 'EE102A', 'EE102B'], 2), # B
@@ -55,7 +79,7 @@ tracks['compeng'] = Req([
         Req([
             'CS205A',
             'CS223A',
-            'ME210', 
+            'ME210',
             'ENGR105',
             Req(['CS225A', 'CS231A', 'ENGR205', 'ENGR207B'], 1)
         ]),
@@ -66,4 +90,191 @@ tracks['compeng'] = Req([
             Req(['CS240', 'CS240E', 'CS241', 'CS244', 'CS244B', 'CS244E', 'CS249A', 'EE179'], 3)
         ])
     ], 1) # C
+])
+
+
+'''
+Biocomputation
+'''
+biocomp_reqA = Req(['CS221', 'CS228', 'CS229', 'CS231A'], 1) # A
+biocomp_reqB = Req(['CS262', 'CS270', 'CS273A', 'CS274', 'CS275', 'CS279'], 1) # B
+biocomp_elec3 = Req([
+    'CS108', 'CS124', 'CS131', 'CS140', 'CS142', 'CS143', 'CS144', 'CS145', 'CS147', 'CS148', 'CS149',
+    'CS154', 'CS155', Req(['CS157', 'PHIL151'], 1), 'CS164', 'CS166', 'CS167', 'CS168', 'CS190',
+    'CS205A', 'CS205B', 'CS210A', 'CS221', 'CS223A', 'CS224N', 'CS224S', 'CS224U', 'CS224W', 'CS225A',
+    'CS227B', 'CS228', 'CS229', 'CS229T', 'CS231A', 'CS231B', 'CS231M', 'CS231N', 'CS232', 'CS233', 'CS240',
+    'CS240H', 'CS242', 'CS243', 'CS244', 'CS244B', 'CS245', 'CS246', 'CS247', 'CS248', 'CS249A', 'CS251',
+    'CS254', 'CS255', 'CS261', 'CS262', 'CS263', 'CS264', 'CS265', 'CS266', 'CS267', 'CS270', 'CS272', 'CS273A',
+    'CS273B', 'CS274', 'CS275', 'CS276', 'CS279', 'CS348B', 'CS348C', 'CS371', 'CS374',
+    'CME108',
+    'EE180', 'EE263', 'EE282', 'EE364A',
+    'BIOE101',
+    'MS&E152', 'MS&E252', 'STATS206', 'STATS315A', 'STATS315B',
+    'BIOMEDIN231', 'BIOMEDIN260',
+    'GENE211'
+], 1)
+biocomp_elec4 = Req([
+    'CS145', 'CS147', 'CS221', 'CS228', 'CS229', 'CS262', 'CS270', 'CS273A', 'CS273B', 'CS274', 'CS275',
+    'CS279', 'CS371', 'CS374',
+    'EE263', 'EE364A',
+    'MS&E152', 'MS&E252',
+    'STATS206', 'STATS315A', 'STATS315B',
+    'BIOMEDIN231', 'BIOMEDIN260',
+    'GENE211'
+], 1)
+biocomp_elec5 = Subreq([biocomp_elec4], [
+    'BIOE222A', 'BIOE222B',
+    'CHEMENG150', 'CHEMENG174',
+    'APPPHYS294',
+    'BIO104', 'BIO118', 'BIO129A', 'BIO129B', 'BIO188', 'BIO189', 'BIO214', 'BIO217', 'BIO230',
+    'CHEM135', 'CHEM171',
+    'BIOC218', 'BIOC241',
+    'SBIO228'
+], 1)
+biocomp_elec6 = Req([
+    'BIOE220', 'BIOE222A', 'BIOE222B',
+    'CHEMENG150', 'CHEMENG174',
+    'CS262', 'CS274', 'CS279', 'CS371', 'CS374',
+    'ME281',
+    'APPPHYS294',
+    'BIO104', 'BIO112', 'BIO118', 'BIO129A', 'BIO129B', 'BIO158', 'BIO183', 'BIO188', 'BIO189', 'BIO214', 'BIO217', 'BIO230',
+    'CHEM135', 'CHEM171',
+    'BIOC218', 'BIOC241',
+    'DBIO210',
+    'GENE211',
+    'SBIO228',
+    'SURG101'
+], 1)
+TRACKS['biocomp'] = Req([
+    Req([
+        Req(['CHEM31A', 'CHEM31B']),
+        'CHEM31X'
+    ], 1),
+    'CHEM33',
+    Req([
+        Req(['BIO41', 'BIO42']),
+        Req(['HUMBIO2A', 'HUMBIO3A', 'HUMBIO4A'])
+    ], 1),
+    biocomp_reqA,
+    biocomp_reqB,
+    Subreq([biocomp_reqA, biocomp_reqB], ['CS124', 'CS145', 'CS147', 'CS148', 'CS248'], 1), # C
+    biocomp_elec3, biocomp_elec4, biocomp_elec5, biocomp_elec6
+])
+
+
+'''
+Human-Computer Interaction
+'''
+hci_reqB = Req(['CS377' + c for c in 'AEWUTVDPMCIBSF'] + [
+    'CS142', 'CS148', 'CS194H', 'CS210A', 'CS376', 'CS448B', 'ME216M'
+], 3)
+TRACKS['hci'] = Req([
+    'CS147',
+    'CS247',
+    hci_reqB
+    # skipping track req C because specifications too vague
+])
+
+
+'''
+Information
+'''
+info_reqB = Req([
+        Req(['CS224N', 'CS224S', 'CS229', 'CS233'], 1),
+        Req(['CS140', 'CS142', 'CS245', 'CS246', 'CS341', 'CS345', 'CS346', 'CS347'], 1),
+        Req(['CS262', 'CS270', 'CS274'], 1),
+        Req(['CS224W', 'CS276'], 1)
+    ], 2)
+TRACKS['info'] = Req([
+    'CS124', # A
+    'CS145', # A
+    info_reqB,
+    Subreq([info_reqB, genelec], [], 3)
+])
+
+
+'''
+Systems
+'''
+sys_reqB = Req(['CS143', 'EE180'], 1)
+sys_reqC = Subreq([sys_reqB], [
+    'CS144', 'CS145', 'CS149', 'CS155', 'CS240', 'CS242', 'CS243', 'CS244', 'CS245', 'EE271', 'EE282'
+], 2)
+TRACKS['systems'] = Req([
+    'CS140', # A
+    sys_reqB,
+    sys_reqC,
+    Subreq([sys_reqC, genelec], [
+        'CS240E', 'CS241', 'CS244E', 'CS316', 'CS341', 'CS343', 'CS344', 'CS345', 'CS346', 'CS347', 'CS349', 'CS448',
+        'EE108', 'EE382C', 'EE384A', 'EE384B', 'EE384C', 'EE384S', 'EE384X'
+    ], 3)
+])
+
+
+'''
+Theory
+'''
+theory_reqB = Req(['CS167', 'CS168', 'CS255', 'CS261', 'CS264', 'CS265', 'CS268'], 1)
+theory_reqC = Subreq([theory_reqB], [
+    'CS143', 'CS155', Req(['CS157', 'PHIL151'], 1), 'CS166', 'CS205A', 'CS228', 'CS233', 'CS242', 'CS250',
+    'CS251', 'CS254', 'CS259', 'CS262', 'CS263', 'CS266', 'CS267', 'CS354', 'CS355', 'CS357', 'CS358',
+    'CS359', 'CS364A', 'CS367', 'CS369', 'CS374', 'MS&E310'
+], 2)
+TRACKS['theory'] = Req([
+    'CS154', # A
+    theory_reqB,
+    theory_reqC,
+    Subreq([theory_reqC, genelec], ['CME302', 'CME305', 'PHIL152'], 3)
+])
+
+
+'''
+Artificial Intelligence
+'''
+ai_reqB = Req([
+    'CS223A',
+    'CS224M',
+    'CS224N',
+    'CS226',
+    'CS227',
+    'CS228',
+    'CS229',
+    Req(['CS131', 'CS231A'], 1)
+], 2)
+ai_reqC = Subreq([ai_reqB], [
+    'CS124', 'CS205A', 'CS224S', 'CS224U', 'CS224W', 'CS225A', 'CS227B', 'CS231A', 'CS231B', 'CS231M', 'CS231N', 'CS262',
+    'CS276', 'CS277', 'CS279', 'CS321', 'CS326A', 'CS327A', 'CS329', 'CS331', 'CS331A', 'CS371', 'CS374', 'CS379',
+    'EE263', 'EE376A',
+    'ENGR205', 'ENGR209A',
+    'MS&E251', 'MS&E351',
+    'STATS315A', 'STATS315B'
+], 1)
+TRACKS['ai'] = Req([
+    'CS221',
+    ai_reqB,
+    ai_reqC,
+    Subreq([ai_reqC, genelec], [
+        'CS238', 'CS275', 'CS278', Req(['CS334A', 'EE364A'], 1),
+        'EE278', 'EE364B',
+        'ECON286',
+        'MS&E252', 'MS&E352', 'MS&E355',
+        'PHIL152',
+        'PSYCH202', 'PSYCH204A', 'PSYCH204B',
+        'STATS200', 'STATS202', 'STATS205'
+    ], 3)
+])
+
+
+'''
+Unspecialized
+'''
+unspecialized_reqB = Req(['CS140', 'CS143'], 1)
+TRACKS['unspecialized'] = Req([
+    'CS154',
+    unspecialized_reqB,
+    Subreq([unspecialized_reqB], ['CS144', 'CS155', 'CS242', 'CS244', 'EE180'], 1), # C
+    Req(['CS221', 'CS223A', 'CS228', 'CS229', 'CS231A'], 1), # D
+    Req(['CS145', 'CS147', 'CS148', 'CS248', 'CS262'], 1), # E
+    genelec,
+    genelec
 ])
--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+from req_builder import Req, Subreq
 
 DATA_DIR = os.getcwd() + '/cs_tracks_notebook_data/'
 MATRIX_FILE = DATA_DIR + 'crs-qtr.vec'
@@ -16,9 +17,11 @@ def load_vocab(path):
         vocab = [w.strip().upper() for w in vocab]
     return vocab
 
+
 def load_rev_vocab(vocab):
     rev_vocab = dict([(x, y) for (y, x) in enumerate(vocab)])
     return rev_vocab
+
 
 def load_courselist(path, limit=None):
     with open(path) as f:
@@ -27,3 +30,104 @@ def load_courselist(path, limit=None):
     if limit:
         return vocab[:limit]
     return vocab
+
+
+'''
+Filter out students from dataframe X that do not fulfill given requirement req
+'''
+def filter_req(X, req, rev_vocab):
+    if isinstance(req, tuple):
+        print(req[0].reqs)
+    if req.all:
+        for r in req.reqs:
+            if isinstance(r, str):
+                if r not in rev_vocab:
+                    print(r, 'not in rev_vocab')
+                    continue
+                courseix = rev_vocab[r]
+                X = X[X.iloc[:,courseix] > 0]
+            elif isinstance(r, (Req, Subreq)):
+                stud_idx = filter_req(X, r, rev_vocab)
+                X = X[X.index.isin(stud_idx)]
+            else:
+                print(type(r), r.reqs)
+                raise TypeError
+        return X.index
+    else:
+        stud_filters = []
+        for r in req.reqs:
+            if isinstance(r, str):
+                if r not in rev_vocab:
+                    print(r, 'not in rev_vocab')
+                    continue
+                courseix = rev_vocab[r]
+                stud_filters.append(X.iloc[:,courseix] > 0)
+            elif isinstance(r, (Req, Subreq)):
+                stud_idx = filter_req(X, r, rev_vocab)
+                stud_filters.append(X.index.isin(stud_idx))
+            else:
+                print(type(r), r.reqs)
+                raise TypeError
+        filter_df = pd.DataFrame(stud_filters).T
+        return X.index[filter_df.sum(axis=1) >= req.num]
+
+
+'''
+Wrapper for filter_req
+'''
+def get_studlist(X, req, filename, rev_vocab):
+    stud_idx = filter_req(X, req, rev_vocab)
+    matches = stud_idx.to_series()
+    return matches
+
+
+'''
+Assigns students uniquely to different tracks
+'''
+def assign_unique_students(tracknames, track_students, track_priorities):
+    track_students_index = {}
+    for t in tracknames:
+        track_students_index[t] = pd.Index(track_students[t].values)
+
+    track_students_unique = {}
+    for t1 in tracknames:
+        idx1 = track_students_index[t1]
+        for t2 in tracknames:
+            if t1 != t2:
+                idx1 = idx1.difference(track_students_index[t2])
+                if track_priorities[t1] < track_priorities[t2]:
+                    overlap = track_students_index[t1].intersection(track_students_index[t2])
+                    idx1 = idx1.union(overlap)
+        track_students_unique[t1] = idx1
+    return track_students_unique
+
+'''
+Count number of courses that deviate from each req (i.e. no. of courses needed to fulfill req)
+'''
+def get_req_deviation(stud_row, req, rev_vocab):
+    dev = 0
+    for r in req.reqs:
+        if isinstance(r, str):
+            if r not in rev_vocab:
+                continue
+            courseix = rev_vocab[r]
+            if stud_row[courseix] <=0:
+                dev += 1
+        elif isinstance(r, Req) or isinstance(r, Subreq):
+            dev += get_req_deviation(stud_row, r, rev_vocab)
+        else:
+            print(type(r), r.reqs)
+            raise TypeError
+    if not req.all:
+        dev -= (len(req.reqs) - req.num) # TODO: debug. if a course is present in several reqs this algorithm looks like it double counts it
+    return max(0, dev)
+
+
+'''
+Count number of courses that deviate from each track (i.e. no. of courses needed to fulfill track)
+'''
+def get_tracks_deviation(stud_row, tracknames, tracks, rev_vocab):
+    tracks_dev = []
+    for t in tracknames:
+        tracks_dev.append(get_req_deviation(stud_row, tracks[t], rev_vocab))
+    return pd.Series(tracks_dev)
