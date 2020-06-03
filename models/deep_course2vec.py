ca273d215d0895af054b239b69f4fa5fd0dbe1e1
alexanderwangus <aswang@stanford.edu>
2020-06-02 19:53:07 -0700

    bugfix

--- models/deep_course2vec.py
+++ models/deep_course2vec.py
@@ -193,7 +193,7 @@ def evaluate_model_bias_single_df(model, df, num_classes_predict=0, categories=F
         X_val['RELATIVE_TERM'] = X_val['RELATIVE_TERM'].apply(util.truncate_class_v2, args=[num_classes_predict])
         X_val['CRSE_GRADE_INPUT'] = X_val['CRSE_GRADE_INPUT'].apply(util.truncate_class_v2, args=[num_classes_predict])
 
-    X_val_lens = util.get_X_lens_v2(X_val, num_classes_predict)
+    X_val_lens = get_X_lens_v2(X_val, num_classes_predict)
     return evaluate_model(X_val, X_val_lens, y, model, output_dict=True, categories=categories, top_n=top_n)
 
 
